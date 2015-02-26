9c8ac20b336175ad1f341feb507b43a1ef95187d
DaniyalAzhar <11bscsdmalik@seecs.edu.pk>
2015-02-26 10:48:02 -0800

    Create myCharacter.java

---
+++ myCharacter.java
@@ -0,0 +1,18 @@
+package interpreter;
+
+public class myCharacter {
+	String str;
+	int value;
+	public String getStr() {
+		return str;
+	}
+	public void setStr(String str) {
+		this.str = str;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
